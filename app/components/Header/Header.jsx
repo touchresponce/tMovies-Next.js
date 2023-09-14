e43b2bc258bbfdcf727578b63ac0bf33d7aa33a2
touchresponce <touchresponce71@gmail.com>
2023-09-14 20:34:01 +0300

    feat: sticky header

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,14 +1,56 @@
+"use client";
+
 import "./Header.css";
-import SearchButton from "../UI/SearchButton/SearchButton";
+import { useEffect, useState } from "react";
 import Navigation from "../Navigation/Navigation";
+import SearchButton from "../UI/SearchButton/SearchButton";
 
 export default function Header() {
+  const [isScroll, setIsScroll] = useState(false);
+  const [isOut, setIsOut] = useState(false);
+  let scrollPrev = typeof window !== "undefined" && window.scrollY;
+
+  // задник шапки
+  function scroll() {
+    if (typeof window !== "undefined") {
+      window.scrollY >= 100 ? setIsScroll(true) : setIsScroll(false);
+    }
+  }
+
+  // спрятать шапку
+  function setHideHeader() {
+    if (typeof window !== "undefined") {
+      window.scrollY >= 50 && window.scrollY > scrollPrev
+        ? setIsOut(true)
+        : setIsOut(false);
+      scrollPrev = window.scrollY;
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", setHideHeader);
+      window.addEventListener("scroll", scroll);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", setHideHeader);
+        window.removeEventListener("scroll", scroll);
+      }
+    };
+  }, []);
+
   return (
-    <header className='header'>
-      <img className='header__logo' alt='логотип сайта' />
-      <div className='header__container'>
-        <Navigation />
-        <SearchButton />
+    <header
+      className={`header ${isScroll ? "scroll" : ""} ${isOut ? "out" : ""}`}
+    >
+      <div className='header__wrapper'>
+        <img className='header__logo' alt='логотип сайта' />
+        <div className='header__container'>
+          <Navigation />
+          <SearchButton />
+        </div>
       </div>
     </header>
   );
